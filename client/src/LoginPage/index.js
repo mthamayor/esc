05b5064291304285953060d8ea7de18b8ffbb1e5
Anifowose Habeeb <anifowosehabeeb@gmail.com>
2019-03-31 12:41:59 +0100

    added client branch

---
+++ client/src/LoginPage/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import { Divider, Avatar } from '@material-ui/core';
+import google from '../assets/svg/Google-color.svg';
+import facebook from '../assets/svg/Facebook-color.svg';
+// relative imports
+import './login-page.css';
+import Styles from '../assets/styles/Styles';
+
+export default () => {
+  return (
+    <div className="container">
+      <div className="row d-flex justify-content-center mt-5">
+        <div className="col-10 col-md-6 col-lg-4">
+          <form>
+            <h4>Log in</h4>
+            <p>Pick up where you left off</p>
+            <TextField
+              id="standard-full-width"
+              label="Email"
+              type="email"
+              helperText=""
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+            <TextField
+              id="standard-full-width"
+              label="Password"
+              type="password"
+              helperText=""
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+            <div className="mt-3">
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                style={Styles.textTransformNone}
+                className="button"
+              >
+                Login
+              </Button>
+            </div>
+            <div className="d-flex justify-content-end mt-2 mb-2">
+              <Link className="a" to="/auth/forgot-password">
+                <small>Forgot password?</small>
+              </Link>
+            </div>
+          </form>
+          <Divider />
+          <div className="text-center mt-3">
+            <p className="">You can also sign in with</p>
+            <div className="d-flex justify-content-center">
+              <Avatar src={google} style={Styles.avatar} />
+              <Avatar src={facebook} />
+            </div>
+            <Divider className="mt-3 mb-3" />
+            <p>
+              Dont have an account?{' '}
+              <Link className="a" to="/auth/signup">
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ client/src/utils/constants/index.js
@@ -0,0 +1,6 @@
+export default {
+  TRANSACTIONS: 'transactions',
+  PROFILE: 'profile',
+  HELP: 'help',
+  NEW_TRANSACTION: 'new-transaction'
+};
